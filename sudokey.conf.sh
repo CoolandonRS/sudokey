ae96c5f6f297198dd30b1664e247ec3cea85426f
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2023-10-29 19:56:35 -0600

    Add config file (#3)
    Moves configuration options into a config file.

---
+++ sudokey.conf.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+readonly confVer=1
+readonly sudokeyFilename="sudo_keys"
+readonly authkeyFilename="authorized_keys"
+readonly sudoFile="/etc/pam.d/sudo"
+readonly sshFile="/etc/ssh/sshd_config"
+readonly scriptLoc="/usr/local/sbin/sudokey"
+readonly keyLine="auth \+sufficient \+pam_ssh_agent_auth.so \+file=~/.ssh/sudo_keys"
+readonly passLine="auth \+include \+system-auth"
+readonly passLine2="password \+include \+system-auth"
+readonly repoLoc="https://raw.githubusercontent.com/CoolandonRS/sudokey/main"
